6c31a778489c369a967091e9c9467f214363096e
dhk10003 <dhk10003@gmail.com>
2023-07-19 21:23:42 -0400

    remigrate

--- js/coding-challenge/CC-02/ReverseString.js
+++
@@ -1,9 +0,0 @@
-function reverse(string) {
-  let result = "";
-  for (let i = string.length - 1; i >= 0; i--) {
-    result += string[i];
-  }
-  return result;
-}
-
-console.log(reverse("Hello World!"));
